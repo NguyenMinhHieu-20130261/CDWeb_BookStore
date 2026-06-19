88f936626c89b5964f78baa94e77f66c7592708c
Nguyen Minh Hieu <20130261@st.hcmuaf.edu.vn>
2026-06-18 16:02:00 -1000

    thêm chức năng đăng nhập admin

--- frontend-admin/src/App.tsx
+++ frontend-admin/src/App.tsx
@@ -1,5 +1,7 @@
 import { Admin, Resource } from "react-admin";
 import dataProvider from "./provider/dataProvider";
+import authProvider from "./provider/authProvider";
+import { LoginPage } from "./layout/Login";
 
 import category from "./components/category";
 import products from "./components/products";
@@ -7,7 +9,11 @@ import blogs from "./components/blogs";
 import users from "./components/users";
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin 
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    loginPage={LoginPage}
+  >
     <Resource name="products" {...products} />
     <Resource name="category" {...category}/>
     <Resource name="blogs" {...blogs}/>
