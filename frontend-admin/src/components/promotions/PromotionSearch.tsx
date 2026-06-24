684286e5811dc390f6bc0cd30a5b110e5d92cc96
TranHungKhanhTam <21130527@st.hcmuaf.edu.vn>
2026-06-25 01:52:05 +0700

    promotion search

---
+++ frontend-admin/src/components/promotions/PromotionSearch.tsx
@@ -0,0 +1,22 @@
+import {
+    SearchInput,
+    SelectInput,
+} from "react-admin";
+import * as React from "react";
+export const PromotionSearch = [
+    <SearchInput
+        key="q"
+        source="q"
+        placeholder="Tìm kiếm mã giảm giá"
+        alwaysOn
+    />,
+    <SelectInput
+        key="status"
+        source="status"
+        label="Trạng thái"
+        choices={[
+            { id: true, name: "Đang hoạt động" },
+            { id: false, name: "Không hoạt động" },
+        ]}
+    />,
+];
