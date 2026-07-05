8d7b1d0b89a832b83407bd7ee9499cf967097542
Nguyen Minh Hieu <20130261@st.hcmuaf.edu.vn>
2026-07-05 03:28:36 -1000

    fix

--- frontend-admin/src/provider/authProvider.tsx
+++ frontend-admin/src/provider/authProvider.tsx
@@ -1,6 +1,7 @@
 import { AuthProvider } from 'react-admin';
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://cdwebbookstore-production.up.railway.app/api";
 
 export const authProvider: AuthProvider = {
     login: ({ username, password }) =>  {
